2146bae4a00b4202f1b1bc753b47241b49c8e0db
Sadhana Nandakumar <snandaku@redhat.com>
2020-03-05 11:23:26 -0500

    removed unnecessary dependency

--- src/main/java/com/redhat/offermanagement/routes/OfferManagementElasticGlue.java
+++ src/main/java/com/redhat/offermanagement/routes/OfferManagementElasticGlue.java
@@ -1,35 +1,9 @@
 package com.redhat.offermanagement.routes;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.elasticsearch.ElasticsearchComponent;
-import org.apache.camel.component.http4.HttpComponent;
-import org.apache.camel.component.kafka.KafkaComponent;
-import org.apache.camel.util.jsse.KeyStoreParameters;
-import org.apache.camel.util.jsse.SSLContextParameters;
-import org.apache.camel.util.jsse.TrustManagersParameters;
-import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.AuthenticationStrategy;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.conn.ssl.*;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
-import org.apache.http.nio.conn.SchemeIOSessionStrategy;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.ssl.SSLContexts;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
-import org.elasticsearch.client.RestHighLevelClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import javax.net.ssl.SSLContext;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.security.KeyStore;
 
 @Component
 public class OfferManagementElasticGlue extends RouteBuilder {
